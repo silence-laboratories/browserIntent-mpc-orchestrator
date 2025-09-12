19372e502f40a8e3a9c6ca8e10c1441f1f352f8a
yogendra-silencelabs <yogendra@silencelaboratories.com>
2025-09-12 11:19:56 +0530

    Feat(bug):using dotenv replacing dotenv-safe

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,5 @@
-import * as dotenv from 'dotenv-safe';
-dotenv.config({
-  allowEmptyValues: true
-});
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const config = {
   port: Number(process.env.PORT) || 3010,
